4722bdf967df721bf3cf1aeb00e3fa77073dfc1c
Choxy5 <kristijan.blazevic000@gmail.com>
2021-10-10 21:31:27 +0200

    Footer component with SCSS

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,5 +2,10 @@ import React from 'react';
 import './Footer.scss';
 
 export function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div>Movie App</div>
+      <div>2021, Movie, Inc. or its affiliates</div>
+    </div>
+  );
 }
