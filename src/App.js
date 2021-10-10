1a7f352f2e0662b8c420a96754b03122333c8f22
Choxy5 <kristijan.blazevic000@gmail.com>
2021-10-10 21:27:00 +0200

    Header component with SCSS

--- src/App.js
+++ src/App.js
@@ -13,11 +13,13 @@ function App() {
     <div className="app">
       <Router>
         <Header></Header>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/movie/:imgbID" component={MovieDetails} />
-          <Route component={PageNotFound} />
-        </Switch>
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/movie/:imgbID" component={MovieDetails} />
+            <Route component={PageNotFound} />
+          </Switch>
+        </div>
         <Footer />
       </Router>
     </div>
