9966b293c5d1ca9612bd46610ef302dcac0f1f57
Choxy5 <kristijan.blazevic000@gmail.com>
2021-10-10 21:54:08 +0200

    Adding Redux to project

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Home.scss';
 
+import { MovieListing } from '../MovieListing/MovieListing';
+
+import movieApi from '../../common/API/movieApi';
+import { APIKey } from '../../common/API/MovieApiKey';
+
 export function Home() {
-  return <div>Home</div>;
+  useEffect(() => {
+    const movieText = 'Harry';
+    async function fetchMovies() {
+      const response = await movieApi
+        .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+        .catch((err) => {
+          console.log(err);
+        });
+      console.log('The response from API', response);
+    }
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      <div className="banner-img"></div>
+      <MovieListing />
+    </div>
+  );
 }
